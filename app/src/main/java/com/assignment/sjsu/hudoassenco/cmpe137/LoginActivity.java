32215c7e2b0f91e0b9125a00a10dbc1442aca4f8
Hudo Assenco <hudo.assenco@gmail.com>
2015-11-17 17:33:40 -0800

    Facebook SDK initialisation and Facebook Login button added. Some refactoring over the string resource.

--- app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137/LoginActivity.java
+++ app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137/LoginActivity.java
@@ -5,15 +5,29 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText _emailText;
     private EditText _passwordText;
     private Button _loginButton;
+    private TextView _signUpLabel;
+    //Facebook stuff
+    private LoginButton _facebookButton;
+    private CallbackManager _facebookCallbackMgr;
+
     private ProgressDialog _progressDialog;
 
     private UserLoginTask _authTask;
@@ -21,11 +35,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Initialize the SDK before executing any other operations,
+        // especially, if you're using Facebook UI elements.
+
         setContentView(R.layout.activity_login);
 
-        _emailText = (EditText) findViewById(R.id.input_email);
-        _passwordText = (EditText) findViewById(R.id.input_password);
-        _loginButton = (Button) findViewById(R.id.btn_login);
+        _emailText = (EditText) findViewById(R.id.login_email_input);
+        _passwordText = (EditText) findViewById(R.id.login_passwd_input);
+        _loginButton = (Button) findViewById(R.id.login_btn);
+        _signUpLabel = (TextView) findViewById(R.id.login_signup_label);
 
         _progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dialog);
         _progressDialog.setIndeterminate(true);
@@ -37,6 +57,40 @@ public class LoginActivity extends AppCompatActivity {
                 attemptLogin();
             }
         });
+        _signUpLabel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Call sign up activity here!
+            }
+        });
+
+        //Facebook stuff initialization
+        _facebookButton = (LoginButton) findViewById(R.id.login_facebook_btn);
+        _facebookCallbackMgr = CallbackManager.Factory.create();
+
+        //TODO: Check other possible permissions.
+        _facebookButton.setReadPermissions("user_friends");
+
+        _facebookButton.registerCallback(_facebookCallbackMgr, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //TODO: Get all user information (Back-end guy)
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e(getString(R.string.log_tag), exception.toString());
+                exception.printStackTrace();
+            }
+        });
+
     }
 
     private void attemptLogin() {
@@ -100,7 +154,6 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
-
             try {
                 // Simulate network access.
                 Thread.sleep(2000);
