1d9c6ae5d6dc34fee4aad19c3f854ba46394e05f
Hudo Assenco <hudo.assenco@gmail.com>
2015-11-30 11:36:08 -0800

    Initial work on Photo Detail Activity

--- app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137/NewsFeedFragment.java
+++ app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137/NewsFeedFragment.java
@@ -1,12 +1,19 @@
 package com.assignment.sjsu.hudoassenco.cmpe137;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeoutException;
 
 public class NewsFeedFragment extends Fragment {
 
@@ -26,6 +33,8 @@ public class NewsFeedFragment extends Fragment {
         mLayoutManager = new LinearLayoutManager(getContext());
         mLadapter = new NewsFeedAdapter();
 
+        //TODO: Back end to query the news feed (Author id, picture link, etc..
+
         mNewsFeedView.setHasFixedSize(true);
         mNewsFeedView.setLayoutManager(mLayoutManager);
         mNewsFeedView.setAdapter(mLadapter);
@@ -35,10 +44,36 @@ public class NewsFeedFragment extends Fragment {
 
     private class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.ViewHolder> {
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-            public ViewHolder(View itemView) {
-                super(itemView);
+            public ImageView mProfilePictureView;
+            public TextView mAlbumNameView;
+            public TextView mAuthorNameView;
+            public TextView mPlacePictureView;
+            public ImageView mPictureView;
+            public TextView mDescriptionView;
+
+            public ViewHolder(View rootView) {
+                super(rootView);
+
+                mProfilePictureView = (ImageView) rootView.findViewById(R.id.feed_profile_picture);
+                mAlbumNameView = (TextView) rootView.findViewById(R.id.feed_album_name);
+                mAuthorNameView = (TextView) rootView.findViewById(R.id.feed_author_name);
+                mPlacePictureView = (TextView) rootView.findViewById(R.id.feed_place_picture);
+                mPictureView = (ImageView) rootView.findViewById(R.id.feed_picture);
+                mDescriptionView = (TextView) rootView.findViewById(R.id.feed_description);
+
+                mPictureView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                if(v.equals(mPictureView)) {
+                    //TODO: Pass a photo identifier to PhotoDetailActivity to query more information.
+                    ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(), mPictureView, "pictureView");
+                    Intent intent = new Intent(getContext(), PhotoDetailActivity.class);
+                    getActivity().startActivity(intent, optionsCompat.toBundle());
+                }
             }
         }
 
---
+++ app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137/PhotoDetailActivity.java
@@ -0,0 +1,80 @@
+package com.assignment.sjsu.hudoassenco.cmpe137;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by hudoassenco on 11/29/15.
+ */
+public class PhotoDetailActivity extends AppCompatActivity {
+
+    private ImageView mPictureView;
+    private ImageButton mLikeBt;
+    private ImageView mProfilePictureView;
+    private TextView mDescriptionView;
+    private TextView mLikesTextView;
+
+    private EditText mCommentEditText;
+    private ImageButton mSendCommentBt;
+
+    private RecyclerView mCommentsListView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_photo_detail);
+
+        mPictureView = (ImageView) findViewById(R.id.photo_detail_photo);
+        mLikeBt = (ImageButton) findViewById(R.id.photo_detail_like_bt);
+        mProfilePictureView = (ImageView) findViewById(R.id.photo_detail_profile_picture);
+        mDescriptionView = (TextView) findViewById(R.id.photo_detail_description);
+        mLikesTextView = (TextView) findViewById(R.id.photo_detail_number_likes);
+
+        mCommentEditText = (EditText) findViewById(R.id.photo_detail_comment_box);
+        mSendCommentBt = (ImageButton) findViewById(R.id.photo_detail_send_bt);
+
+        mCommentsListView = (RecyclerView) findViewById(R.id.photo_detail_comments_list);
+
+        mLikeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLikeBtClicked();
+            }
+        });
+        mSendCommentBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSendCommentBtClicked();
+            }
+        });
+
+        //TODO: Back end to query informations about the photo, comments, likes, etc.
+    }
+
+    private void onSendCommentBtClicked() {
+        String comment = mCommentEditText.getText().toString();
+        //TODO: Back end to save comment.
+    }
+
+    private void onLikeBtClicked() {
+        if(isUserLikedPicture()) {
+            //TODO: Back end to unlike the picture
+            mLikeBt.setImageResource(R.drawable.thumbs_outline);
+        } else {
+            //TODO: Back end to like the picture
+            mLikeBt.setImageResource(R.drawable.thumbs_fill);
+        }
+    }
+
+    private boolean isUserLikedPicture() {
+        //TODO: back end to query if the user liked the picture
+        return true;
+    }
+}
